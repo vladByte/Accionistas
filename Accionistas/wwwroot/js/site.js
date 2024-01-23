3e1f5c80cc48c5769feb54c1431b95c667141138
vladByte <vlad.jerez@gmail.com>
2024-01-22 21:59:22 -0400

    agregando inicializacion de script

--- Accionistas/wwwroot/js/site.js
+++ Accionistas/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    // Asocia la función al clic del botón
+    $("#miBoton").on("click", function () {
+        CartaPoder();
+    });
+});
